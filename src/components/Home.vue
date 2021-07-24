5c59a2fbb2b44c12c4ac965a8a5aff2b6951d2c0
Matthew Klein <mklein994@gmail.com>
2021-07-24 13:05:10 -0500

    Show list of links on homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,1 +1,24 @@
-<template>home</template>
+<template>
+  <ul>
+    <li v-for="link of links" :key="link.path">
+      <RouterLink :to="link.path">{{ link.name }}</RouterLink>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+
+import { routes } from "../routes";
+
+export default defineComponent({
+  name: "Home",
+  setup: () => {
+    const links = computed(() => routes.filter((x) => x.name !== undefined));
+
+    return {
+      links,
+    };
+  },
+});
+</script>
