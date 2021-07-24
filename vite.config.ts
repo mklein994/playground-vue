7699d903a724fccde51d8ed0b91c7b11db4effcb
Matthew Klein <mklein994@gmail.com>
2021-07-24 13:49:17 -0500

    Setup autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: { postcss: { plugins: [autoprefixer()] } },
   plugins: [vue()],
 });
