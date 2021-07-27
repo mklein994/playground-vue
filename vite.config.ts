dd7b371daa9749ea215a2ad98f1967566df975ce
Matthew Klein <mklein994@gmail.com>
2021-07-27 13:46:56 -0500

    Fix import sort order in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import autoprefixer from "autoprefixer";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
