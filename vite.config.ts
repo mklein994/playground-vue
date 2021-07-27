f1a38a2a442124cc03ea72eb6a305b8970715834
Matthew Klein <mklein994@gmail.com>
2021-07-27 13:34:11 -0500

    Serve on 127.0.0.1
    For some reason, Firefox on Android doesn't understand "localhost"—at
    least, not without passing `--open` to vite, and even then, only if
    using that browser tab.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { host: "127.0.0.1" },
   css: { postcss: { plugins: [autoprefixer()] } },
   plugins: [vue()],
 });
