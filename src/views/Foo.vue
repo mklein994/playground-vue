63b93b13b632b18def0117957cd3e09017f8e5a7
Matthew Klein <mklein994@gmail.com>
2021-08-01 16:37:35 -0500

    Add Foo view component
    This is simply a smoke test; just to see if everything is working.

---
+++ src/views/Foo.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Foo</h1>
+  <p>
+    Adipisicing nisi et explicabo voluptatum quisquam Quia expedita quae
+    explicabo eveniet dolorum? Eligendi repellendus perferendis provident
+    voluptatum similique Distinctio ipsam dignissimos vero sint aperiam rerum
+    Veniam expedita quos fuga velit.
+  </p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Foo",
+});
+</script>
