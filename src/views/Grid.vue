b7def2e091dc170c04e621605fa9aebd615f2ad7
Matthew Klein <mklein994@gmail.com>
2021-08-02 23:15:08 -0500

    Create a simple 'Grid' component

---
+++ src/views/Grid.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="grid-component">
+    <div v-for="fruit in fruits" :key="fruit" class="item">{{ fruit }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "Grid",
+  setup: () => {
+    const fruits = ref(["apple", "banana", "cherry"]);
+
+    return {
+      fruits,
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.grid-component {
+  display: grid;
+  gap: 1rem;
+  margin: 1rem;
+  padding: 1rem;
+  background: lightcoral;
+}
+
+.grid-component > {
+  .item {
+    border: 1px solid black;
+  }
+}
+</style>
