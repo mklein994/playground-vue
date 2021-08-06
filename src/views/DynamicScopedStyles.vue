560ac43c95f01a7686767e7ba120aa9e7be91c6c
Matthew Klein <mklein994@gmail.com>
2021-08-05 20:15:08 -0500

    Test dynamically added styles with 'scoped'

---
+++ src/views/DynamicScopedStyles.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button @click="toggleClass">Toggle Class</button>
+    <p :class="fooClass">
+      Elit labore optio quia explicabo sunt blanditiis saepe. Doloribus
+      architecto rerum consectetur ducimus placeat! Accusamus voluptate ut
+      laborum ipsam vero? Voluptates est vero perspiciatis repellat numquam
+      Nulla aperiam natus maiores
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "DynamicScopedStyles",
+  setup() {
+    const showRedClass = ref(false);
+
+    const toggleClass = () => (showRedClass.value = !showRedClass.value);
+
+    const fooClass = computed(() =>
+      showRedClass.value ? "red-class" : "blue-class"
+    );
+
+    return {
+      fooClass,
+      toggleClass,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.red-class {
+  color: red;
+}
+
+.blue-class {
+  color: blue;
+}
+</style>
