b2074fc225e9245ee14b30546dd13a896667eb1d
Matthew Klein <mklein994@gmail.com>
2021-08-06 22:49:29 -0500

    Address vite file system access warning

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { host: "127.0.0.1" },
+  server: {
+    fs: {
+      // TODO: this setting is experimental. Once
+      // stabilized, it's expected that this will be
+      // `true`, at which point it can be removed.
+      strict: true,
+    },
+    host: "127.0.0.1",
+  },
   plugins: [vue()],
 });
