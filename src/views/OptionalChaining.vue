14bb98bff6fb28ac4ddcbe138e1a83f8493706b0
Matthew Klein <mklein994@gmail.com>
2021-08-10 17:34:47 -0500

    Add experiment for optional chaining

---
+++ src/views/OptionalChaining.vue
@@ -0,0 +1,31 @@
+<template>
+  <strong>Fruit:</strong>
+  <pre>{{ fruits }}</pre>
+  <strong>Stores:</strong>
+  <pre>{{ stores }}</pre>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "OptionalChaining",
+
+  setup: () => {
+    const fruits = ref([
+      { id: "apple", name: "Apple", store: { name: "Superstore" } },
+      { id: "banana", name: "Banana" },
+      { id: "cherry", name: "Cherry", store: { name: "Costco" } },
+    ]);
+
+    const stores = computed(() =>
+      fruits.value.map((x) => x.store?.name ?? "(unknown store)")
+    );
+
+    return {
+      fruits,
+      stores,
+    };
+  },
+});
+</script>
