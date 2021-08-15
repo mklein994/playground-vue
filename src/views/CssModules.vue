9511182c7e3bb9c60f3aca6de18eb4423f8e5847
Matthew Klein <mklein994@gmail.com>
2021-08-15 12:34:19 -0500

    Add a CSS Modules experiment

---
+++ src/views/CssModules.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="$style.foo">
+    Ipsum rem optio quia sequi minus! Voluptatibus dolorem deleniti repellat
+    adipisci ab optio doloremque Culpa facere enim voluptatum quia ad? Odit enim
+    in quis beatae recusandae Enim porro commodi iusto?
+  </div>
+  <pre>{{ foo }}</pre>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, useCssModule } from "vue";
+
+export default defineComponent({
+  name: "CssModules",
+
+  setup: () => {
+    const css = useCssModule();
+
+    const foo = ref<string>(css.foo);
+
+    return {
+      foo,
+    };
+  },
+});
+</script>
+
+<style module>
+.foo {
+  color: red;
+}
+</style>
