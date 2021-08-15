69b3a3235614c679889477183b06bb5adac3c777
Matthew Klein <mklein994@gmail.com>
2021-08-14 21:15:29 -0500

    Replace basename regex with string functions
    Also, fix some misnomers for variable names.

--- src/routes.ts
+++ src/routes.ts
@@ -6,15 +6,20 @@ const modules = import.meta.glob("./views/*.vue");
 
 const componentRoutes: RouteRecordRaw[] = [];
 
-for (const key in modules) {
-  const basename = key.replace(/^.*\/([^/]+)\.vue$/, "$1");
-  const snakeCase = basename.replace(/\B([A-Z])/g, "-$1").toLowerCase();
-  const titleCase = snakeCase.replace(/-/g, " ");
+for (const path in modules) {
+  const basename =
+    // import.meta.glob and import.meta.globEager will always show paths that
+    // contain a slash, since they are "absolute", i.e. relative to the project
+    // root (location of vite.config.js).
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    path.split("/").pop()!.slice(0, -".vue".length);
+  const kebabCase = basename.replace(/\B([A-Z])/g, "-$1").toLowerCase();
+  const titleCase = kebabCase.replace(/-/g, " ");
 
   componentRoutes.push({
-    path: `/${snakeCase}`,
+    path: `/${kebabCase}`,
     name: titleCase,
-    component: modules[key],
+    component: modules[path],
   });
 }
 
