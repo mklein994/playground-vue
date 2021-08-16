600f413aaca7c1d34f54feb4d6dd14126039f377
Matthew Klein <matthew.klein@birchwood.ca>
2021-08-16 10:36:44 -0500

    Add an experiment for v-bind() in <style> tags

---
+++ src/views/CssVBind.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text">hello</div>
+  <input v-model="color" type="text" name="color" />
+  <input v-model="color" type="color" name="color" />
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const color = ref("blue");
+</script>
+
+<style scoped>
+.text {
+  /* can be a JavaScript expression, e.g. v-bind("color ?? 'blue'") */
+  color: v-bind("color");
+}
+</style>
