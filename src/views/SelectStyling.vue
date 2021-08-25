42e61b8b2cd3f7053301656ee40aa15eb877922b
Matthew Klein <mklein994@gmail.com>
2021-08-25 14:39:54 -0500

    Create an experiment for styling <select> elements

---
+++ src/views/SelectStyling.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="select-styling">
+    <div
+      v-for="classes of ['default', 'none']"
+      :key="classes"
+      class="select-wrapper"
+    >
+      <select class="select" :class="classes">
+        <option disabled value="">Choose a fruit:</option>
+        <option>Apple</option>
+        <option>Banana</option>
+        <option>Cherry</option>
+      </select>
+      <span>{{ classes }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.select-styling {
+  background-color: rgb(25 25 0 / 0.1);
+}
+
+.none {
+  appearance: none;
+  background: none;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  outline: 0;
+}
+</style>
