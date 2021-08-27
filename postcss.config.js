49fcb952881cf393e51f043fa039416d033e5623
Matthew Klein <mklein994@gmail.com>
2021-08-26 22:29:23 -0500

    Move postcss config to vite.config.ts

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    "postcss-logical": { preserve: true },
-    "postcss-dir-pseudo-class": {},
-  },
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import vue from "@vitejs/plugin-vue";
+import autoprefixer from "autoprefixer";
+import postcssDirPseudoClass from "postcss-dir-pseudo-class";
+import postcssLogical from "postcss-logical";
+import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+        postcssLogical({ preserve: true }),
+        postcssDirPseudoClass,
+      ],
+    },
+  },
   server: {
     fs: {
       // TODO: this setting is experimental. Once
