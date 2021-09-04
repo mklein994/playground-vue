f531ea3403d1d519197fd182006ec748bafeb83b
Matthew Klein <mklein994@gmail.com>
2021-09-03 23:17:40 -0500

    Highlight code block with highlight.js

--- src/views/OptionalChaining.vue
+++ src/views/OptionalChaining.vue
@@ -6,10 +6,22 @@
   <pre
     style="font-size: 0.8em; white-space: pre-wrap"
   ><code>{{ getStores }}</code></pre>
+  <Highlightjs
+    language="ts"
+    :code="getStores.toString()"
+    style="white-space: pre-wrap"
+  />
 </template>
 
 <script lang="ts" setup>
+import hljsVuePlugin from "@highlightjs/vue-plugin";
+import hljs from "highlight.js/lib/core";
+import typescript from "highlight.js/lib/languages/typescript";
 import { computed, ref } from "vue";
+
+hljs.registerLanguage("ts", typescript);
+
+const Highlightjs = hljsVuePlugin.component;
 
 const fruits = ref([
   { id: "apple", name: "Apple", store: { name: "Superstore" } },
@@ -22,3 +34,7 @@ const getStores = () =>
 
 const stores = computed(getStores);
 </script>
+
+<style>
+@import "highlight.js/styles/github-dark-dimmed.css";
+</style>
