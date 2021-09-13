cc0af74c9ed483cb759038c25c86d0c799db9c77
Matthew Klein <mklein994@gmail.com>
2021-09-12 22:23:51 -0500

    Silence eslint warnings for postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+
 module.exports = {
   plugins: [
     require("tailwindcss"),
     require("autoprefixer"),
     require("postcss-logical")({ preserve: true }),
     require("postcss-dir-pseudo-class"),
-  ]
+  ],
 };
