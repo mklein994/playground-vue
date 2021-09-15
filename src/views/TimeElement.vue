bec442cc8aa1398b0c0edbb36f5c2b12fa16684a
Matthew Klein <mklein994@gmail.com>
2021-09-14 19:30:03 -0500

    Add a <time> element experiment

---
+++ src/views/TimeElement.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="time-element">
+    <time datetime="PT5M">5 minutes</time>
+
+    <p>
+      An experiment to see how assistive technologies interact with the
+      <code class="code">&lt;time&gt;</code> element.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.time-element {
+  margin: 1rem;
+}
+
+.code {
+  font-size: 0.9rem;
+}
+</style>
