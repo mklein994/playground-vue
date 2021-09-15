dac757e5c9708c23907240074f78866ab5215147
Matthew Klein <mklein994@gmail.com>
2021-09-14 19:16:24 -0500

    Add a sass experiment

---
+++ src/views/Sass.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="sass">
+    This is highlighted in <code class="code">theme("colors.red.500")</code>.
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+$primary-color: theme("colors.red.500");
+
+.sass {
+  color: $primary-color;
+}
+
+.code {
+  font-size: 1rem;
+
+  &::before,
+  &::after {
+    content: "`";
+  }
+}
+</style>
