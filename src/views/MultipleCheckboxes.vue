dddc76b4486bc18519e4eebd23283045012acbf8
Matthew Klein <mklein994@gmail.com>
2021-10-02 13:45:42 -0500

    Add an example for multiple related checkboxes

---
+++ src/views/MultipleCheckboxes.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="multiple-checkboxes">
+    <div class="checkboxes">
+      <template v-for="fruit of fruits" :key="fruit.id">
+        <input
+          :id="fruit.id"
+          v-model="selectedFruits"
+          type="checkbox"
+          :value="fruit.id"
+          class="checkbox"
+        />
+
+        <label :for="fruit.id" class="label">{{ fruit.name }}</label>
+      </template>
+    </div>
+
+    <pre>{{ selectedFruits }}</pre>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const fruits = [
+  { id: "apple", name: "Apple" },
+  { id: "banana", name: "Banana" },
+  { id: "cherry", name: "Cherry" },
+];
+
+const selectedFruits = ref<string[]>([]);
+</script>
+
+<style scoped>
+.multiple-checkboxes {
+  padding: 1em;
+}
+
+.checkboxes {
+  display: grid;
+  grid: auto / auto 1fr;
+  gap: 0.25em 0.125em;
+}
+</style>
