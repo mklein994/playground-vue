22fc5657fea28cfa8eaeb81753b44e7c18412c2c
Matthew Klein <mklein994@gmail.com>
2021-10-08 20:30:43 -0500

    Add sticky headers to XOR table :D

--- src/views/XorOperation.vue
+++ src/views/XorOperation.vue
@@ -1,46 +1,40 @@
 <template>
-  <table class="xor-operation">
-    <thead class="header">
-      <tr>
-        <th class="cell">key</th>
-        <th class="cell">P</th>
-        <th class="cell">Q</th>
-        <th class="cell">R</th>
-        <th class="cell">S</th>
-        <th class="cell">t1</th>
-        <th class="cell">t2</th>
-        <th class="cell">result</th>
-      </tr>
-    </thead>
-    <tbody class="body">
-      <tr
-        v-for="{ key, p, q, r, s, t1, t2, result } of map"
-        :key="key"
-        class="row"
-      >
-        <td class="cell">{{ key }}</td>
-        <td class="cell">{{ p }}</td>
-        <td class="cell">{{ q }}</td>
-        <td class="cell">{{ r }}</td>
-        <td class="cell">{{ s }}</td>
-        <td class="cell" :class="t1 ? 'true' : 'false'">{{ t1 }}</td>
-        <td class="cell" :class="t2 ? 'true' : 'false'">{{ t2 }}</td>
-        <td class="cell" :class="result ? 'true' : 'false'">{{ result }}</td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="table-wrapper">
+    <table class="xor-operation">
+      <thead class="header">
+        <tr class="row">
+          <th class="cell">key</th>
+          <th class="cell">P</th>
+          <th class="cell">Q</th>
+          <th class="cell">R</th>
+          <th class="cell">S</th>
+          <th class="cell">t1</th>
+          <th class="cell">t2</th>
+          <th class="cell">result</th>
+        </tr>
+      </thead>
+      <tbody class="body">
+        <tr
+          v-for="{ key, p, q, r, s, t1, t2, result } of map"
+          :key="key"
+          class="row"
+        >
+          <td class="cell">{{ key }}</td>
+          <td class="cell">{{ p }}</td>
+          <td class="cell">{{ q }}</td>
+          <td class="cell">{{ r }}</td>
+          <td class="cell">{{ s }}</td>
+          <td class="cell" :class="t1 ? 'true' : 'false'">{{ t1 }}</td>
+          <td class="cell" :class="t2 ? 'true' : 'false'">{{ t2 }}</td>
+          <td class="cell" :class="result ? 'true' : 'false'">{{ result }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 
-  <Highlightjs
-    language="ts"
-    :code="test1.toString()"
-    style="white-space: no-wrap"
-  />
+  <Highlightjs language="ts" :code="test1Code" class="code" />
 
-  <Highlightjs
-    language="ts"
-    :code="test2.toString()"
-    style="white-space: no-wrap"
-  />
+  <Highlightjs language="ts" :code="test2Code" class="code" />
 </template>
 
 <script lang="ts" setup>
@@ -97,17 +91,46 @@ const map = data.map((row) => {
 
   return { key, ...row, t1, t2, result };
 });
+
+const test1Code = `${test1}`.replaceAll(/\n {4}/g, "\n");
+const test2Code = `${test2}`.replaceAll(/\n {4}/g, "\n");
 </script>
 
 <style scoped>
+.table-wrapper {
+  margin: 1em;
+  overflow: auto;
+}
+
 .xor-operation {
-  border-collapse: collapse;
+  border-collapse: separate;
+  border-spacing: 0;
 }
 
 .cell {
   font-family: monospace;
   font-size: 1rem;
   padding-inline: 0.5em;
+  border: 1px solid lightgray;
+}
+
+.header .cell {
+  position: sticky;
+  inset-block-start: 0;
+  border-block-end: 2px solid black;
+  background: white;
+}
+
+.cell:first-child {
+  position: sticky;
+  inset-inline-start: 0;
+  background: white;
+  border-inline-end: 2px solid black;
+  z-index: 1;
+}
+
+.header .cell:first-child {
+  z-index: 2;
 }
 
 .cell.true {
@@ -116,6 +139,10 @@ const map = data.map((row) => {
 
 .cell.false {
   color: red;
+}
+
+.code {
+  white-space: pre;
 }
 </style>
 
