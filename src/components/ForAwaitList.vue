fad0aa82363f973687d54b288ecc8e0295999c26
Matthew Klein <mklein994@gmail.com>
2021-10-22 13:26:18 -0500

    Create a FizzBuzz demo using for await

---
+++ src/components/ForAwaitList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="for-await-list">
+    <label for="max">Max:</label>
+    <input id="max" v-model="max" type="number" name="max" min="1" />
+
+    <ol>
+      <li v-for="(value, index) of list" :key="`${value}${index}`">
+        <span :class="{ 'is-number': Number.isInteger(value) }">{{
+          value
+        }}</span>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, watchEffect } from "vue";
+
+async function* iteratorGenerator(max: number) {
+  let i = 1;
+  while (i <= max) {
+    yield i++;
+  }
+}
+
+async function fizzBuzz(max: number) {
+  const list = [];
+  for await (let i of iteratorGenerator(max)) {
+    list.push(
+      i % 15 === 0
+        ? "FizzBuzz"
+        : i % 3 === 0
+        ? "Fizz"
+        : i % 5 === 0
+        ? "Buzz"
+        : i
+    );
+  }
+  return list;
+}
+
+const max = ref(25);
+
+const list = ref<(string | number)[]>([]);
+
+watchEffect(async () => {
+  list.value = await fizzBuzz(max.value);
+});
+</script>
+
+<style scoped>
+.is-number {
+  color: dodgerblue;
+}
+</style>
---
+++ src/views/ForAwait.vue
@@ -0,0 +1,13 @@
+<template>
+  <Suspense>
+    <ForAwaitList />
+  </Suspense>
+</template>
+
+<script lang="ts" setup>
+import { defineAsyncComponent } from "vue";
+
+const ForAwaitList = defineAsyncComponent(
+  () => import("../components/ForAwaitList.vue")
+);
+</script>
