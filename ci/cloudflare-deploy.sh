76e2b6dee2aa14acdd6ba05f3f91450f5d98d9f1
Matthew Klein <matthew.klein@birchwood.ca>
2021-10-28 09:59:42 -0500

    ci: Echo comments to Cloudflare build log

--- ci/cloudflare-deploy.sh
+++ ci/cloudflare-deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# setup rust with wasm32-unknown-unknown
+echo 'setup rust with wasm32-unknown-unknown'
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --target wasm32-unknown-unknown -y
 
-# add rust to $PATH
+echo 'add rust to PATH'
 source "${HOME}/.cargo/env"
 
-# setup wasm-pack
+echo 'setup wasm-pack'
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 
-# clone sunrise-cli repo
+echo 'clone sunrise-cli repo'
 git -c protocol.version=2 clone --no-tags --no-recurse-submodules --depth=1 https://github.com/mklein994/sunrise-cli ../sunrise-cli
 
-# build wasm
+echo 'build wasm'
 wasm-pack build --target web ../sunrise-cli
 
-# build vue project
+echo 'build vue project'
 corepack pnpm run build
