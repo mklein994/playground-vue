4796bafb95a7bbdaa3d5359e5682be9f8324eed6
Matthew Klein <mklein994@gmail.com>
2021-10-27 20:54:01 -0500

    ci: Add rust to path

--- ci/cloudflare-deploy.sh
+++ ci/cloudflare-deploy.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --target wasm32-unknown-unknown -y
+source "${HOME}/.cargo/env"
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 git clone https://github.com/mklein994/sunrise-cli ../sunrise-cli
 wasm-pack build --target web ../sunrise-cli
