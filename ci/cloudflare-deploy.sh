13c81ba6ff3db165bb7bb6f7caefec30e36b57bc
Matthew Klein <mklein994@gmail.com>
2021-10-27 20:44:07 -0500

    ci: Add a build script for cloudflare

---
+++ ci/cloudflare-deploy.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --target wasm32-unknown-unknown -y
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+git clone https://github.com/mklein994/sunrise-cli ../sunrise-cli
+wasm-pack build --target web ../sunrise-cli
+corepack pnpm run build
