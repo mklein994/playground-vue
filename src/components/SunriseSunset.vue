bd4cef59ae35559f7a277ea60a44dc8cd6625938
Matthew Klein <mklein994@gmail.com>
2021-10-31 18:37:22 -0500

    SunriseSunset: Add stricter prop validation

--- src/components/SunriseSunset.vue
+++ src/components/SunriseSunset.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from "vue";
+import { computed, PropType } from "vue";
 
 import init, { get_sunrise_sunset } from "../../../sunrise-cli/pkg/sunrise_cli";
 
@@ -14,11 +14,23 @@ interface Coord {
   lon: number;
 }
 
-const props = defineProps<{
-  coords: Coord;
-  date: string;
-  azimuth: string;
-}>();
+const props = defineProps({
+  coords: {
+    type: Object as PropType<Coord>,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+    validator: (value: string) => /^\d{4}-\d{2}-\d{2}$/.test(value),
+  },
+  azimuth: {
+    type: String,
+    required: true,
+    validator: (value: string) =>
+      ["Official", "Civil", "Nautical", "Astronomical"].includes(value),
+  },
+});
 
 await init();
 
