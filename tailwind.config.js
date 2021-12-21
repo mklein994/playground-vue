3a5a8a42c4edb42d4434aae882ef0a2e24c392a4
Matthew Klein <mklein994@gmail.com>
2021-12-20 23:03:50 -0600

    Update tailwind config for v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,7 @@
-// eslint-disable-next-line no-undef, @typescript-eslint/no-var-requires
-const colors = require("tailwindcss/colors");
-
 // eslint-disable-next-line no-undef
 module.exports = {
   prefix: "tw-",
-  mode: "jit",
-  purge: ["./index.html", "./src/**/*.{vue,css,ts,html}"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        sky: colors.sky,
-      },
-    },
-  },
-  variants: {
-    extend: {},
-  },
+  content: ["./index.html", "./src/**/*.{vue,css,ts,html}"],
   // eslint-disable-next-line no-undef
   plugins: [require("@tailwindcss/forms")],
 };
