ced4257dfb156282c1a8ce4fcde40575639747cd
Matthew Klein <matthew.klein@birchwood.ca>
2022-01-17 13:54:29 -0600

    Add ConsoleTrace experiment

---
+++ src/views/ConsoleTrace.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="clickMe">Click Me</button>
+</template>
+
+<script lang="ts" setup>
+const otherFunc = () => {
+  console.log("From otherFunc...");
+  console.trace("Made it.");
+};
+
+function clickMe(event: MouseEvent) {
+  console.groupCollapsed("dump");
+  console.log(event);
+  console.log("from clickMe…");
+  otherFunc();
+  console.groupEnd();
+}
+</script>
