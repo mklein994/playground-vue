1e35eba76d9d0b1266953f8d220bf776762919cc
Matthew Klein <mklein994@gmail.com>
2022-01-22 16:12:06 -0600

    Add an about page to find browser info

---
+++ src/views/AboutBrowser.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about-grid">
+    <span>User Agent</span>
+    <span>{{ userAgent }}</span>
+    <span>Inner Width</span>
+    <span>{{ inner }}</span>
+    <span>Outer Width</span>
+    <span>{{ outer }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+
+const userAgent = navigator.userAgent;
+const inner = computed(() => ({
+  width: window.innerWidth,
+  height: window.innerHeight,
+}));
+
+const outer = computed(() => ({
+  width: window.outerWidth,
+  height: window.outerHeight,
+}));
+</script>
+
+<style scoped>
+.about-grid {
+  display: grid;
+  grid: auto-flow / auto auto;
+  justify-content: center;
+  width: 100%;
+  gap: 0.5em 2em;
+
+  font-family: monospace;
+}
+</style>
