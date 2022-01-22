d4b9a7efa5464c53e2fcba40e5aaaf4c6548a5b6
Matthew Klein <mklein994@gmail.com>
2022-01-22 16:41:30 -0600

    Add a page for throwing an error

---
+++ src/views/ThrowError.vue
@@ -0,0 +1,9 @@
+<template>
+  <button @click="throwError()">Throw an error</button>
+</template>
+
+<script lang="ts" setup>
+const throwError = () => {
+  throw new Error("Aha! You found me!");
+};
+</script>
