25f72fa4679c3e2190e3c469fea09e0b9127975b
Matthew Klein <matthew.klein@birchwood.ca>
2022-02-09 18:34:37 -0600

    Add an experiment for custom events

---
+++ src/components/EventCallbacksHelper.vue
@@ -0,0 +1,39 @@
+<template>
+  <button @click="doStuff">Do Stuff</button>
+  <output
+    :style="{
+      color:
+        success === true ? 'green' : success === undefined ? undefined : 'red',
+    }"
+    >{{ result }}</output
+  >
+</template>
+
+<script lang="ts" setup>
+import { ref, watchEffect } from "vue";
+
+const props = defineProps<{
+  success?: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: "stuff"): number;
+}>();
+
+const result = ref("result before");
+
+const doStuff = () => {
+  emit("stuff");
+};
+
+watchEffect(
+  function () {
+    if (props.success !== undefined) {
+      return props.success ? console.log("success!") : console.warn("fail!");
+    }
+  },
+  { flush: "sync" }
+);
+
+watchEffect(() => console.log(result.value));
+</script>
---
+++ src/views/EventCallbacks.vue
@@ -0,0 +1,19 @@
+<template>
+  <EventCallbacksHelper @stuff="handleDoStuff" :success="success" />
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import EventCallbacksHelper from "../components/EventCallbacksHelper.vue";
+
+const success = ref();
+
+const handleDoStuff = async () => {
+  console.log("waiting...");
+
+  await new Promise(x => setTimeout(x, 1000));
+
+  console.log("made it");
+  success.value = true;
+};
+</script>
