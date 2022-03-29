9da6c6b6fbd5449c7ede1b8fb7f93b318d1cdd72
Matthew Klein <matthew.klein@birchwood.ca>
2022-03-29 16:33:36 -0500

    Start testing simple views and components

--- tests/SimpleViews.spec.ts
+++ tests/SimpleViews.spec.ts
@@ -1,11 +1,19 @@
 import { mount } from "@vue/test-utils";
 import { describe, expect, it } from "vitest";
 
+import NotFound from "@/NotFound.vue";
 import FooExperiment from "@/views/FooExperiment.vue";
 
 describe("FooExperiment", () => {
   it("renders correctly", () => {
     const wrapper = mount(FooExperiment);
     expect(wrapper.text()).toContain("Foo");
+  });
+});
+
+describe("NotFound", () => {
+  it("renders correctly", () => {
+    const wrapper = mount(NotFound);
+    expect(wrapper.html()).toContain(`<h1>404 Not Found</h1>`);
   });
 });
