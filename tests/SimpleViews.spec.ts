33e63259cda88ef68f265e5e16694eb6544a0b57
Matthew Klein <mklein994@gmail.com>
2022-03-30 01:00:00 -0500

    Test CustomElement.ce.vue

--- tests/SimpleViews.spec.ts
+++ tests/SimpleViews.spec.ts
@@ -1,6 +1,7 @@
 import { mount } from "@vue/test-utils";
 import { describe, expect, it } from "vitest";
 
+import CustomElement from "@/components/CustomElement.ce.vue";
 import HomeView from "@/HomeView.vue";
 import NotFound from "@/NotFound.vue";
 import FooExperiment from "@/views/FooExperiment.vue";
@@ -9,6 +10,11 @@ describe.each([
   ["FooExperiment", FooExperiment, "Foo"],
   ["NotFound", NotFound, "404 Not Found"],
   ["HomeView", HomeView, "Welcome"],
+  [
+    "CustomElement",
+    CustomElement,
+    Array.from({ length: 10 }, (_, i) => `Item ${i + 1}`).join(""),
+  ],
 ])("%s", (_name, component, text) => {
   it("renders correctly", () => {
     expect(component).toBeTruthy();
