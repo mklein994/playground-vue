ed9ddaacc2fdb9bacb74ae9b50efaaa53a43a6bf
Matthew Klein <mklein994@gmail.com>
2022-04-21 10:53:49 -0500

    Make all tests concurrent and test ForAwait max

--- src/use/smokeTest.ts
+++ src/use/smokeTest.ts
@@ -1,7 +1,7 @@
 if (import.meta.vitest) {
   const { describe, expect, it } = import.meta.vitest;
 
-  describe("smokeTest", () => {
+  describe.concurrent("smokeTest", () => {
     it("checks that tests are working", () => {
       expect(1 + 2).toBe(3);
     });
--- tests/ForAwait.spec.ts
+++ tests/ForAwait.spec.ts
@@ -4,32 +4,64 @@ import { defineComponent, h, Suspense } from "vue";
 
 import ForAwaitList from "@/components/ForAwaitList.vue";
 
-describe("ForAwait", async () => {
-  const TestComponent = defineComponent(function TestComponent() {
-    return () => h(Suspense, () => h(ForAwaitList));
+describe.concurrent("ForAwait", async () => {
+  const TestComponentBuilder = (max?: number) =>
+    defineComponent(function TestComponent() {
+      return () =>
+        h(Suspense, () => h(ForAwaitList, max == null ? undefined : { max }));
+    });
+
+  describe.concurrent("all in order", async () => {
+    const wrapper = mount(TestComponentBuilder());
+
+    await flushPromises();
+
+    it.each([
+      [1, "1"],
+      [2, "2"],
+      [3, "Fizz"],
+      [4, "4"],
+      [5, "Buzz"],
+      [6, "Fizz"],
+      [7, "7"],
+      [8, "8"],
+      [9, "Fizz"],
+      [10, "Buzz"],
+      [11, "11"],
+      [12, "Fizz"],
+      [13, "13"],
+      [14, "14"],
+      [15, "FizzBuzz"],
+      [16, "16"],
+    ])("fizzbuzz: %i -> %s", (index, expected) => {
+      expect(wrapper.get(`li:nth-child(${index})`).text()).toBe(expected);
+    });
   });
-  const wrapper = mount(TestComponent);
 
-  await flushPromises();
+  describe.concurrent("max", () => {
+    it("handles a different initial value", async () => {
+      const wrapper = mount(TestComponentBuilder(10));
 
-  it.each([
-    [1, "1"],
-    [2, "2"],
-    [3, "Fizz"],
-    [4, "4"],
-    [5, "Buzz"],
-    [6, "Fizz"],
-    [7, "7"],
-    [8, "8"],
-    [9, "Fizz"],
-    [10, "Buzz"],
-    [11, "11"],
-    [12, "Fizz"],
-    [13, "13"],
-    [14, "14"],
-    [15, "FizzBuzz"],
-    [16, "16"],
-  ])("fizzbuzz: %i -> %s", (index, expected) => {
-    expect(wrapper.get(`li:nth-child(${index})`).text()).toBe(expected);
+      await flushPromises();
+
+      expect(wrapper.get("li:last-child").text()).toBe("Buzz");
+    });
+
+    it("updates when changed", async () => {
+      const wrapper = mount(TestComponentBuilder(10));
+
+      await flushPromises();
+
+      const last = () => wrapper.get("li:last-child");
+      expect(wrapper.findAll("li")).toHaveLength(10);
+      expect(last().text()).toBe("Buzz");
+
+      await wrapper.get("#max").setValue(3);
+
+      await flushPromises();
+
+      expect(wrapper.findAll("li")).toHaveLength(3);
+      expect(last().text()).toBe("Fizz");
+    });
   });
 });
