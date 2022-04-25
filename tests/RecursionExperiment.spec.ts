60c8b048d2278be2c4a7041af11480f0f7b9f32c
Matthew Klein <matthew.klein@birchwood.ca>
2022-04-25 12:45:22 -0500

    Wrap RecursionExperiment test in a describe() block

--- tests/RecursionExperiment.spec.ts
+++ tests/RecursionExperiment.spec.ts
@@ -1,25 +1,27 @@
 import { flushPromises, mount } from "@vue/test-utils";
-import { expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 
 import RecursionExperiment from "@/views/RecursionExperiment.vue";
 
-it.concurrent("RecursionExperiment", async () => {
-  const wrapper = mount(RecursionExperiment, {
-    props: {
-      source: [
-        "store.produce.apple",
-        "store.produce.banana",
-        "store.produce.cherry",
-        "person.male.dad",
-        "person.male.son",
-        "person.female.mom",
-        "person.female.daughter",
-      ],
-      split: (word: string) => word.split("."),
-      search: "store.produce.apple",
-    },
-  });
-  await flushPromises();
+describe.concurrent("RecursionExperiment", () => {
+  it("renders correctly", async () => {
+    const wrapper = mount(RecursionExperiment, {
+      props: {
+        source: [
+          "store.produce.apple",
+          "store.produce.banana",
+          "store.produce.cherry",
+          "person.male.dad",
+          "person.male.son",
+          "person.female.mom",
+          "person.female.daughter",
+        ],
+        split: (word: string) => word.split("."),
+        search: "store.produce.apple",
+      },
+    });
+    await flushPromises();
 
-  expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper.html()).toMatchSnapshot();
+  });
 });
