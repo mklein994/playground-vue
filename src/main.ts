7a4f7d340190f02cc78c434b8926b82b15cf9165
Matthew Klein <matthew.klein@birchwood.ca>
2022-04-28 10:53:48 -0500

    Move sentry setup to separate file

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,10 @@ import "./fonts.css";
 // import "./tailwind.css";
 import "./style.css";
 
-import { Integrations } from "@sentry/tracing";
-import * as Sentry from "@sentry/vue";
-import { Wasm as WasmIntegration } from "@sentry/wasm";
 import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
+
+import { sentry } from "@/use/sentry";
 
 import App from "./App.vue";
 import { routes } from "./routes";
@@ -19,21 +18,6 @@ const router = createRouter({
 
 const app = createApp(App);
 
-const dsn = import.meta.env.VITE_SENTRY_DSN;
-if (dsn !== undefined) {
-  Sentry.init({
-    app,
-    dsn,
-    integrations: [
-      new Integrations.BrowserTracing({
-        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-        tracingOrigins: ["localhost", "playground-vue.pages.dev", /^\//],
-      }),
-      new WasmIntegration(),
-    ],
-    tracesSampleRate: 1.0,
-    release: import.meta.env.VITE_SENTRY_RELEASE,
-  });
-}
+app.use(sentry, { router });
 
 app.use(router).mount("#app");
---
+++ src/use/sentry.ts
@@ -0,0 +1,28 @@
+import { Integrations } from "@sentry/tracing";
+import * as Sentry from "@sentry/vue";
+import { Wasm as WasmIntegration } from "@sentry/wasm";
+import type { App } from "vue";
+import type { Router } from "vue-router";
+
+export const sentry = {
+  install: (app: App, { router }: { router: Router }) => {
+    const dsn = import.meta.env.VITE_SENTRY_DSN;
+    if (dsn === undefined) {
+      return;
+    }
+
+    Sentry.init({
+      app,
+      dsn,
+      integrations: [
+        new Integrations.BrowserTracing({
+          routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+          tracingOrigins: ["localhost", "playground-vue.pages.dev", /^\//],
+        }),
+        new WasmIntegration(),
+      ],
+      tracesSampleRate: 1.0,
+      release: import.meta.env.VITE_SENTRY_RELEASE,
+    });
+  },
+};
