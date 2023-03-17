de3faf123bdd87ba265eed6a21d8eb6264cbb99f
Matthew Klein <matthew.klein@birchwood.ca>
2023-03-17 11:24:02 -0500

    Add a pass-by-reference experiment

---
+++ src/views/PassByReference.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const foo = {
+  bar: {
+    baz: 5,
+  },
+};
+
+const fooCopy = JSON.parse(JSON.stringify(foo));
+
+const bar = foo.bar;
+bar.baz = 6;
+</script>
+
+<template>
+  <div class="pass-by-reference">
+    <pre><strong>fooCopy:</strong> {{ fooCopy }}</pre>
+    <pre><strong>foo:</strong> {{ foo }}</pre>
+  </div>
+</template>
