06424302f528835006c1659bffcd371446a23835
Matthew Klein <mklein994@gmail.com>
2023-07-08 15:18:06 -0500

    Fix AudioPicker component types
    This also limits files it accepts to just audio files.

--- src/components/AudioPicker.vue
+++ src/components/AudioPicker.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onBeforeUnmount, ref } from "vue";
+import { onBeforeUnmount, ref } from "vue";
 
 const props = defineProps<{
   modelValue: string | undefined;
@@ -9,34 +9,40 @@ const emit = defineEmits<{
   "update:modelValue": [url: string | undefined];
 }>();
 
-const fileUrl = ref<string | undefined>(props.modelValue);
+const file = ref<File | null>();
 
-const audioUrl = computed({
-  get() {
-    return fileUrl.value;
-  },
-  set(value: File | undefined) {
-    if (fileUrl.value) {
-      URL.revokeObjectURL(fileUrl.value);
-    }
-    fileUrl.value = value ? URL.createObjectURL(value) : undefined;
-    emit("update:modelValue", fileUrl.value);
-  },
-});
+// NOTE: this does not react when the prop changes, since
+// props are not reactive. That should be fine for now
+// though, since this component is where we get the URL
+// from, not the caller. It just makes it simpler to use
+// v-model instead of using a custom event.
+const url = ref<string | undefined>(props.modelValue);
 
 const handleFileChange = (event: Event) => {
   const target = event.target as HTMLInputElement;
-  const file = target.files?.item(0);
-  audioUrl.value = file;
+  file.value = target.files?.item(0);
+
+  if (url.value) {
+    URL.revokeObjectURL(url.value);
+  }
+
+  if (file.value) {
+    url.value = URL.createObjectURL(file.value);
+  } else {
+    url.value = undefined;
+  }
+
+  emit("update:modelValue", url.value);
 };
 
 onBeforeUnmount(() => {
-  if (fileUrl.value) {
-    URL.revokeObjectURL(fileUrl.value);
+  if (url.value) {
+    URL.revokeObjectURL(url.value);
+    emit("update:modelValue", undefined);
   }
 });
 </script>
 
 <template>
-  <input type="file" @change="handleFileChange" />
+  <input type="file" accept="audio/*" @change="handleFileChange" />
 </template>
