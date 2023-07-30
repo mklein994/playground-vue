7f1337b1e18678ef19ed4d402663c4c059a7c093
Matthew Klein <mklein994@gmail.com>
2023-07-30 14:06:48 -0500

    Add an experiment for focus, hover, and active states

---
+++ src/views/InteractivityStates.vue
@@ -0,0 +1,227 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const message = ref<string>();
+
+const handleClick = (event: Event) => {
+  const target = event.target as HTMLButtonElement;
+  message.value = `clicked: ${target.textContent}`;
+};
+
+const fruits = [
+  { name: "Apple", value: "apple" },
+  { name: "Banana", value: "banana" },
+  { name: "Cherry", value: "cherry" },
+];
+</script>
+
+<template>
+  <div class="interactivity-states">
+    <div class="button-groups">
+      <div class="button-group">
+        <button type="button" class="button first-button" @click="handleClick">
+          First Button
+        </button>
+        <button type="button" class="button second-button" @click="handleClick">
+          Second Button
+        </button>
+      </div>
+
+      <div class="button-group">
+        <button
+          type="button"
+          class="button first-button custom"
+          @click="handleClick"
+        >
+          First Custom Button
+        </button>
+        <button
+          type="button"
+          class="button second-button custom"
+          @click="handleClick"
+        >
+          Second Custom Button
+        </button>
+      </div>
+      <output>{{ message }}</output>
+    </div>
+
+    <div class="text-groups">
+      <div class="text-group">
+        <label for="first-text">First Text</label>
+        <input id="first-text" type="text" class="text first-text" />
+
+        <label for="second-text">Second Text</label>
+        <input id="second-text" type="text" class="text second-text" />
+      </div>
+
+      <div class="text-group">
+        <label for="first-custom-text">First Custom Text</label>
+        <input
+          id="first-custom-text"
+          type="text"
+          class="text first-text custom"
+        />
+
+        <label for="second-custom-text">Second Custom Text</label>
+        <input
+          id="second-custom-text"
+          type="text"
+          class="text second-text custom"
+        />
+      </div>
+    </div>
+
+    <div class="radio-groups">
+      <div class="radio-group">
+        <template v-for="fruit of fruits" :key="fruit.name">
+          <label :for="`${fruit.name}-${fruit.value}`">{{ fruit.name }}</label>
+          <input
+            :id="`${fruit.name}-${fruit.value}`"
+            type="radio"
+            name="fruit"
+            :value="fruit.value"
+            class="radio"
+          />
+        </template>
+      </div>
+
+      <div class="radio-group">
+        <template v-for="fruit of fruits" :key="fruit.name">
+          <label :for="`${fruit.name}-${fruit.value}-custom`"
+            >Custom {{ fruit.name }}</label
+          >
+          <input
+            :id="`${fruit.name}-${fruit.value}-custom`"
+            type="radio"
+            name="fruit-custom"
+            :value="fruit.value"
+            class="radio custom"
+          />
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.interactivity-states {
+  display: grid;
+  padding: 1em;
+  gap: 1em;
+}
+
+.button-groups {
+  display: grid;
+  gap: 1em;
+}
+
+.button-group {
+  display: flex;
+  gap: 1em;
+}
+
+.text-groups {
+  display: grid;
+  justify-content: start;
+  gap: 1em;
+}
+
+.text-group {
+  display: grid;
+  gap: 1em;
+  grid: auto-flow / repeat(2, 1fr);
+}
+
+.radio-groups {
+  display: grid;
+}
+
+.radio-group {
+  display: grid;
+  align-items: baseline;
+  justify-content: start;
+  column-gap: 1em;
+  grid: auto-flow / repeat(2, auto);
+}
+
+.custom {
+  appearance: none;
+}
+
+.radio.custom {
+  --size: 0.85rem;
+  --ratio: 0.5;
+
+  /*
+  These colors come from inspecting Firefox defaults with the color picker.
+  */
+  --default-color: #8f8f9d;
+  --hover-color: #676774;
+  --active-color: #484851;
+
+  --checked-default-color: #3584e4;
+  --checked-hover-color: #1e6fc5;
+  --checked-active-color: #0059a8;
+
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  padding: 0 2px;
+  border: 2px solid var(--default-color);
+  border-radius: 50%;
+  margin: 3px 3px 0 5px;
+  block-size: var(--size);
+  inline-size: var(--size);
+}
+
+.radio.custom::after {
+  block-size: calc(var(--size) * var(--ratio));
+  content: "";
+  inline-size: calc(var(--size) * var(--ratio));
+}
+
+.radio.custom:hover {
+  border-color: var(--hover-color);
+}
+
+.radio.custom:active {
+  border-color: var(--active-color);
+}
+
+.radio.custom:focus {
+  outline: 2px solid lightskyblue;
+  outline-offset: 1px;
+}
+
+.radio.custom:checked {
+  border-color: var(--checked-default-color);
+}
+
+/* Support browsers that don't support :focus-visible */
+.radio.custom:focus:not(:focus-visible) {
+  outline: unset;
+  outline-offset: unset;
+}
+
+.radio.custom:checked:hover {
+  border-color: var(--checked-hover-color);
+}
+
+.radio.custom:checked:active {
+  border-color: var(--checked-active-color);
+}
+
+.radio.custom:checked::after {
+  border-radius: inherit;
+  background-color: var(--checked-default-color);
+}
+
+.radio.custom:checked:hover::after {
+  background-color: var(--checked-hover-color);
+}
+
+.radio.custom:checked:active::after {
+  background-color: var(--checked-active-color);
+}
+</style>
