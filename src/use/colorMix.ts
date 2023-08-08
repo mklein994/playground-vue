b936f22c9be54b61739d7f434ae7140c27986ffc
Matthew Klein <mklein994@gmail.com>
2023-08-08 01:22:53 -0500

    Refactor color-mix experiment
    This fixes a couple of issues:
    - The computed background color text was wrong if you changed the color
    - The computed background color text was too small on desktop

--- src/use/colorMix.ts
+++ src/use/colorMix.ts
@@ -25,3 +25,41 @@ export const flatColorSpaces = [
 ] as const;
 
 export type ColorSpace = (typeof flatColorSpaces)[number];
+
+const generateGradientPercents = (stops: number) => {
+  const percents: number[] = [];
+  const increment = 1 / (stops + 1);
+  for (let i = 0; i < stops + 2; i++) {
+    const percent = i * increment;
+    percents.push(percent);
+  }
+  return percents;
+};
+
+export const generateGradient = (
+  colorSpace: ColorSpace,
+  firstColor: string,
+  lastColor: string,
+  stops: number,
+) =>
+  generateGradientPercents(stops).map(
+    (stop) =>
+      `color-mix(in ${colorSpace}, ${firstColor}, ${lastColor} ${(
+        stop * 100
+      ).toFixed(2)}%)`,
+  );
+
+if (import.meta.vitest) {
+  const { describe, expect, it } = import.meta.vitest;
+
+  describe("generateGradientPercents", () => {
+    it.each([
+      [0, [0 / 1, 1 / 1]],
+      [1, [0 / 2, 1 / 2, 2 / 2]],
+      [2, [0 / 3, 1 / 3, 2 / 3, 3 / 3]],
+      [3, [0 / 4, 1 / 4, 2 / 4, 3 / 4, 4 / 4]],
+    ])("handles %i stops", (stops, expected) => {
+      expect(generateGradientPercents(stops)).toStrictEqual(expected);
+    });
+  });
+}
