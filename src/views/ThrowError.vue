c0b4462ef31336a1ee07a706a6848bd0e2d40d51
Matthew Klein <matthew.klein@birchwood.ca>
2023-10-19 15:49:27 -0500

    Experiment with nested error causes

--- src/views/ThrowError.vue
+++ src/views/ThrowError.vue
@@ -1,9 +1,33 @@
 <script setup lang="ts">
-const throwError = () => {
-  throw new Error("Aha! You found me!");
+const divideNumbers = (a: number, b: number) => {
+  try {
+    const result = a / b;
+    if (!Number.isFinite(result)) {
+      throw new Error("Result is infinite");
+    }
+    return result;
+  } catch (error: unknown) {
+    throw new Error("Failed to divide these numbers", {
+      cause: {
+        error,
+        values: { a, b },
+      },
+    });
+  }
+};
+
+const handleClick = () => {
+  try {
+    const result = divideNumbers(1, 0);
+    console.log(result);
+  } catch (error: unknown) {
+    throw new Error("Aha! You found me!", {
+      cause: error,
+    });
+  }
 };
 </script>
 
 <template>
-  <button @click="throwError()">Throw an error</button>
+  <button @click="handleClick()">Throw an error</button>
 </template>
