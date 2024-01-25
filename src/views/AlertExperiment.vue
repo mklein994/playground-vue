38ece5a39e1c1da3ac51ee83fe7084a7100457ea
Matthew Klein <mklein994@gmail.com>
2024-01-25 13:06:44 -0600

    Add an experiment for alerts and notifications

---
+++ src/views/AlertExperiment.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const openAlert = () => {
+  alert("Hey there!");
+};
+
+const hasPermission = computed(() => Notification.permission === "granted");
+
+const openNotification = async () => {
+  if (!hasPermission.value) {
+    await Notification.requestPermission();
+  }
+
+  new Notification("Title", {
+    body: "Body",
+  });
+};
+</script>
+
+<template>
+  <button type="button" @click="openAlert">Open Alert</button>
+  <button type="button" @click="openNotification">Open Notification</button>
+</template>
