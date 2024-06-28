528f0e53622acb07a50d74f5f28f928b57da8013
Matthew Klein <mklein994@gmail.com>
2024-06-28 12:17:21 -0500

    Add an experiment for relative colors

---
+++ src/views/RelativeColorSyntax.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import hljsVuePlugin from "@highlightjs/vue-plugin";
+import hljs from "highlight.js/lib/core";
+import css from "highlight.js/lib/languages/css";
+
+hljs.registerLanguage("css", css);
+
+const Highlightjs = hljsVuePlugin.component;
+
+const code = `
+@supports (color: rgb(from white r g b)) {
+  .foo {
+    color: rgb(from white r g b);
+  }
+}
+`;
+</script>
+
+<template>
+  <div class="relative-color-syntax">
+    <p>Syntax goes like this:</p>
+    <Highlightjs lang="css" :code="code" />
+
+    <p>Demo:</p>
+
+    <div class="demo">
+      <h1>Demo</h1>
+
+      <p class="outer">
+        Elit perferendis dolor natus cupiditate unde! Distinctio assumenda
+        aspernatur ad enim eligendi! <span class="inner">Foobar</span> Nihil
+        molestias hic vitae soluta vel. Provident odio eum labore odit nobis Ad
+        neque deserunt omnis porro hic.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.demo {
+  --color: green;
+  color: green;
+}
+
+.outer {
+  color: rgb(from var(--color) r g 128);
+}
+
+.inner {
+  color: hsl(from var(--color) calc(h + 180) s l);
+}
+</style>
