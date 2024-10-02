1bd86d2e490a7304df148a123de6239e812b1fcf
Matthew Klein <mklein994@gmail.com>
2024-10-02 09:06:21 -0500

    Add a cursor experiment

---
+++ src/views/CursorExperiment.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+const cursors = [
+  "auto",
+  "default",
+  "none",
+  "context-menu",
+  "help",
+  "pointer",
+  "progress",
+  "wait",
+  "cell",
+  "crosshair",
+  "text",
+  "vertical-text",
+  "alias",
+  "copy",
+  "move",
+  "no-drop",
+  "not-allowed",
+  "grab",
+  "grabbing",
+  "all-scroll",
+  "col-resize",
+  "row-resize",
+  "n-resize",
+  "e-resize",
+  "s-resize",
+  "w-resize",
+  "ne-resize",
+  "nw-resize",
+  "se-resize",
+  "sw-resize",
+  "ew-resize",
+  "ns-resize",
+  "nesw-resize",
+  "nwse-resize",
+  "zoom-in",
+  "zoom-out",
+];
+</script>
+
+<template>
+  <div class="cursor-experiment">
+    <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/cursor"
+      >https://developer.mozilla.org/en-US/docs/Web/CSS/cursor</a
+    >
+    <div class="cursors">
+      <code
+        v-for="cursor of cursors"
+        :key="cursor"
+        class="cursor"
+        :style="{ cursor }"
+        >cursor: {{ cursor }};</code
+      >
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cursor-experiment {
+  display: grid;
+  padding: 1rem;
+  gap: 1em;
+}
+
+.cursors {
+  display: grid;
+}
+
+.cursor {
+  display: block;
+  padding: 0.25em;
+}
+</style>
