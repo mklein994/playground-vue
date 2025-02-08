c71566fbb2884440390ce20d9a9c8ad78655fd93
Matthew Klein <mklein994@gmail.com>
2025-02-08 13:48:22 -0600

    Add an advanced debounce experiment
    This adds a `debouncedRef` function, which can optionally handle
    "leading" and "trailing" scenarios.

---
+++ src/use/debouncedRef.ts
@@ -0,0 +1,122 @@
+import type { MaybeRefOrGetter, Ref } from "vue";
+import { customRef, ref, toValue } from "vue";
+
+// Inspired by https://learnersbucket.com/examples/interview/debouncing-with-leading-and-trailing-options/
+const debounce = <T extends unknown[], U>(
+  fn: (...args: T) => U,
+  wait = 0,
+  options: { leading?: boolean; trailing?: boolean } = {},
+): ((...args: T) => void) => {
+  const { leading = false, trailing = true } = options;
+
+  let timeout: ReturnType<typeof setTimeout> | undefined;
+  let isLeadingInvoked = false;
+
+  return function <V>(this: V, ...args: T) {
+    clearTimeout(timeout);
+
+    // leading
+    if (leading && !timeout) {
+      fn.apply(this, args);
+      isLeadingInvoked = true;
+    } else {
+      isLeadingInvoked = false;
+    }
+
+    // trailing
+    timeout = setTimeout(() => {
+      if (trailing && !isLeadingInvoked) {
+        fn.apply(this, args);
+      }
+
+      timeout = undefined;
+    }, wait);
+  };
+};
+
+export const debouncedRef = <T>(
+  initialValue?: MaybeRefOrGetter<T>,
+  wait = 250,
+  options: { leading?: boolean; trailing?: boolean } = {},
+): Ref<T> => {
+  return customRef((track, trigger) => {
+    const inner = ref(toValue(initialValue)) as Ref<T>;
+
+    const realUpdate = (newValue: T) => {
+      inner.value = newValue;
+      trigger();
+    };
+
+    const debouncedUpdate = debounce(realUpdate, wait, options);
+
+    const update = wait === 0 ? realUpdate : debouncedUpdate;
+
+    return {
+      get() {
+        track();
+        return inner.value;
+      },
+      set(newValue) {
+        update(newValue);
+      },
+    };
+  });
+};
+
+if (import.meta.vitest) {
+  const { afterEach, beforeEach, describe, expect, it, vi } = import.meta
+    .vitest;
+
+  describe("debouncedRef", () => {
+    beforeEach(() => {
+      vi.useFakeTimers();
+    });
+
+    afterEach(() => {
+      vi.useRealTimers();
+    });
+
+    it("has an initial value of undefined when not given any", () => {
+      const debounced = debouncedRef();
+      expect(debounced.value).toBeUndefined();
+    });
+
+    it("uses the initial value immediately", () => {
+      const debounced = debouncedRef("hello");
+      expect(debounced.value).toStrictEqual("hello");
+    });
+
+    it("updates the value immediately if a wait duration of 0 was given", () => {
+      const debounced = debouncedRef("hello", 0);
+      debounced.value = "goodbye";
+      expect(debounced.value).toStrictEqual("goodbye");
+    });
+
+    it("updates the value after a delay if given a wait duration", () => {
+      const debounced = debouncedRef("hello", 250);
+      debounced.value = "goodbye";
+      expect(debounced.value).toStrictEqual("hello");
+      vi.advanceTimersByTime(249);
+      expect(debounced.value).toStrictEqual("hello");
+      vi.advanceTimersByTime(1);
+      expect(debounced.value).toStrictEqual("goodbye");
+    });
+
+    it("updates the value immediately if leading is true with a wait duration", () => {
+      const debounced = debouncedRef("hello", 250, { leading: true });
+      debounced.value = "goodbye";
+      expect(debounced.value).toStrictEqual("goodbye");
+    });
+
+    it("updates the value after a delay if updated again before the wait duration", () => {
+      const debounced = debouncedRef("hello", 250, { leading: true });
+      debounced.value = "goodbye";
+      vi.advanceTimersByTime(1);
+      debounced.value = "farewell";
+      vi.advanceTimersByTime(1);
+      expect(debounced.value).toStrictEqual("goodbye");
+      vi.advanceTimersByTime(249);
+      expect(debounced.value).toStrictEqual("farewell");
+    });
+  });
+}
---
+++ src/views/AdvancedDebounce.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { ref, shallowReactive, watch } from "vue";
+
+import { debouncedRef } from "@/use/debouncedRef";
+
+const wait = ref(250);
+const leading = ref(false);
+const trailing = ref(true);
+const changed = ref(false);
+watch(
+  [wait, leading, trailing],
+  ([newWait, newLeading, newTrailing], [oldWait, oldLeading, oldTrailing]) => {
+    if (
+      newWait !== oldWait ||
+      newLeading !== oldLeading ||
+      newTrailing !== oldTrailing
+    ) {
+      changed.value = true;
+    }
+  },
+);
+const nameRef = shallowReactive({
+  name: debouncedRef("hello", wait.value, {
+    leading: leading.value,
+    trailing: trailing.value,
+  }),
+});
+
+const handleSubmit = () => {
+  nameRef.name = debouncedRef("hello", wait.value, {
+    leading: leading.value,
+    trailing: trailing.value,
+  });
+  changed.value = false;
+};
+</script>
+
+<template>
+  <form class="settings" @submit.prevent="handleSubmit">
+    <label for="wait">Wait (ms)</label>
+    <input id="wait" v-model="wait" type="number" />
+
+    <label for="leading">Leading</label>
+    <input id="leading" v-model="leading" type="checkbox" />
+
+    <label for="trailing">Trailing</label>
+    <input id="trailing" v-model="trailing" type="checkbox" />
+
+    <button type="submit" :disabled="!changed">Submit</button>
+  </form>
+  <label for="name">Name</label>
+  <input
+    id="name"
+    v-model="nameRef.name.value"
+    type="text"
+    :disabled="changed"
+  />
+  <pre>{{ nameRef.name }}</pre>
+</template>
+
+<style scoped>
+.settings {
+  display: grid;
+  grid-template-columns: auto auto;
+  gap: 0.5rem;
+  justify-content: start;
+  justify-items: start;
+  margin: 1rem;
+  max-width: max-content;
+}
+</style>
