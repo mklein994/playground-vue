82c98543f8fcf46243da2922769cda8d3d8fdca6
Matthew Klein <mklein994@gmail.com>
2025-05-11 20:42:52 -0500

    No longer need to await on node tests (Node v24)

--- tests/smoke-test.node-test.js
+++ tests/smoke-test.node-test.js
@@ -2,12 +2,12 @@ import test from "node:test";
 
 import { strict as assert } from "assert";
 
-test("node tests", async (t) => {
-  await t.test("subtest 1", () => {
+test("node tests", (t) => {
+  t.test("subtest 1", () => {
     assert.strictEqual(3, 1 + 2);
   });
 
-  await t.test("subtest 2", { skip: true }, () => {
+  t.test("subtest 2", { skip: true }, () => {
     const before = { name: "Bob", age: 24 };
     const after = { name: "Bob", age: 25 };
     assert.deepEqual(before, after);
