43a90482343e6cc587576cafdbe0f8050967b495
Matthew Klein <mklein994@gmail.com>
2025-05-30 13:31:54 -0500

    Update postcss.config.js to optionally build with Tailwind support

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,16 @@
+import process from "node:process";
+import fs from "node:fs";
+
+let tailwindSupported = true;
+if (fs.existsSync("./.env.local")) {
+  process.loadEnvFile("./.env.local");
+  tailwindSupported = process.env.BUILDTIME_TAILWIND_SUPPORTED === "true";
+}
+
 export default {
   plugins: {
     // "tailwindcss/nesting": "postcss-nesting",
-    "@tailwindcss/postcss": {},
+    "@tailwindcss/postcss": tailwindSupported ? {} : false,
     autoprefixer: {},
     "postcss-logical": {},
     "postcss-dir-pseudo-class": {},
