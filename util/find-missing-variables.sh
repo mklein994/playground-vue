ff78177f5cdc7c3ae570e84a529e40a9d1fd95ba
Matthew Klein <mklein994@gmail.com>
2025-09-21 16:21:35 -0500

    Find only missing/unused base (i.e. generated) tokens

--- util/find-missing-variables.sh
+++ util/find-missing-variables.sh
@@ -4,7 +4,7 @@ pnpm run build:tokens -p json >&2
 
 comm -13 <(jq -r 'keys[] | "--\(.)"' src/assets/generated/variables.json | sort -u) \
   <(git grep --untracked --no-heading -h --only-matching \
-    --perl-regexp -e '--pv-\w[-\w]+' \
+    --perl-regexp -e '--pv-base-\w[-\w]+' \
     ':/**/*.scss' \
     ':/**/*.css' \
     ':/**/*.vue' |
