f26a7a90e5032fa683658220081a660bf6446ed8
Matthew Klein <mklein994@gmail.com>
2025-09-21 15:39:08 -0500

    Refactor sd.config.ts from a build script to a config

--- sd.config.ts
+++ sd.config.ts
@@ -1,44 +1,15 @@
-import chroma from "chroma-js";
 import type { Config } from "style-dictionary";
-import StyleDictionary from "style-dictionary";
 import { formats, transforms } from "style-dictionary/enums";
 import { fileURLToPath, URL } from "url";
 
-import { compactHex, expandHex, shortColorName } from "./src/tokens/helpers.ts";
-
 const resolve = (path: string) => fileURLToPath(new URL(path, import.meta.url));
 
-const cssTransforms = [transforms.nameKebab];
-
-StyleDictionary.registerTransform({
-  name: "custom/value/color/short",
-  type: "value",
-  filter(token) {
-    return token.$type === "color";
-  },
-  transform(token, _options) {
-    const color = chroma(token.$value as string);
-    const hex = color.num();
-    const name = shortColorName(hex);
-    if (name) {
-      return name;
-    }
-
-    const compact = compactHex(hex);
-    if (hex === expandHex(compact)) {
-      return `#${compact.toString(16).padStart(3, "0")}`;
-    }
-
-    return `#${hex.toString(16).padStart(6, "0")}`;
-  },
-});
-
-const config: Config = {
+export default {
   source: [resolve("./src/tokens/base/**/*.ts")],
 
   platforms: {
     css: {
-      transforms: cssTransforms,
+      transforms: [transforms.nameKebab],
       prefix: "pv",
       buildPath: resolve("./src/assets/generated/"),
 
@@ -47,7 +18,15 @@ const config: Config = {
           format: formats.cssVariables,
           destination: "variables.css",
         },
+      ],
+    },
 
+    json: {
+      transforms: [transforms.nameKebab],
+      prefix: "pv",
+      buildPath: resolve("./src/assets/generated/"),
+
+      files: [
         {
           format: formats.jsonFlat,
           destination: "variables.json",
@@ -55,7 +34,4 @@ const config: Config = {
       ],
     },
   },
-};
-
-const sd = new StyleDictionary(config);
-await sd.buildAllPlatforms();
+} satisfies Config;
