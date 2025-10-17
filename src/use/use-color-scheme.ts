ad606a93b0251a355b4547846fe048a506d13ad3
Matthew Klein <mklein994@gmail.com>
2025-10-17 17:11:32 -0500

    Change default tailwind form style in dark mode

--- src/use/use-color-scheme.ts
+++ src/use/use-color-scheme.ts
@@ -56,6 +56,10 @@ export const useColorScheme = () => {
     }
 
     meta.setAttribute("content", metaContent);
+    document.documentElement.classList.toggle(
+      "tw-dark",
+      resolvedScheme.value === "dark",
+    );
   });
 
   return {
