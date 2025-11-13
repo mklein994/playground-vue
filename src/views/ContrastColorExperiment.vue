5ee1f7473f622af62643c8d0061506dfd0694757
Matthew Klein <mklein994@gmail.com>
2025-11-13 14:24:36 -0600

    Add a contrast color experiment
    This doesn't use the `contrast-color()` function, since most browsers
    (other than Safari) don't support it yet.

---
+++ src/views/ContrastColorExperiment.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const color = ref("red");
+</script>
+
+<template>
+  <div class="contrast-color-experiment">
+    <form class="form" @submit.prevent>
+      <input v-model="color" type="color" />
+      <input id="color" v-model="color" type="text" />
+      <label for="color">Color</label>
+    </form>
+
+    <p class="sample-text pv-contrast-color">Hello world!</p>
+  </div>
+</template>
+
+<style>
+.contrast-color-experiment {
+  .sample-text {
+    --color: v-bind("color");
+    background-color: var(--color);
+    color: var(--pv-contrast-color);
+  }
+}
+</style>
