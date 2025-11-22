1f3756330ab1c44caaabecdb8c7303038cd3d183
Matthew Klein <mklein994@gmail.com>
2025-11-22 15:06:57 -0600

    Add a utility class to show <abbr> elements when tapped on mobile

---
+++ src/views/AbbrMobileExperiment.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="abbr-mobile-experiment">
+    <p>
+      Dolor cupiditate temporibus dolore doloribus labore Alias nobis molestiae
+      rerum incidunt illo Unde molestiae impedit magnam quisquam vitae Id iure
+      quidem nisi? Tap this abbreviation
+      <abbr title="As Soon As Possible" class="pv-mobile-abbr">ASAP</abbr>.
+    </p>
+  </div>
+</template>
+
+<style>
+.abbr-mobile-experiment {
+  padding: 1rem;
+}
+</style>
