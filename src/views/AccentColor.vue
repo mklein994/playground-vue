8a353dcd332618cbb70a690bf277e24c4cc6c244
Matthew Klein <mklein994@gmail.com>
2025-12-03 10:47:10 -0600

    Add tailwind classes to accent color fields, more input types, and an indeterminate toggle

--- src/views/AccentColor.vue
+++ src/views/AccentColor.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 
 const fruits = [
   { id: "apple", name: "Apple", checked: false },
@@ -14,23 +14,48 @@ const selectedFruitSelect = ref(selectedFruits.value[0]);
 const color = ref("rebeccapurple");
 const checkbox = ref<HTMLInputElement>();
 
+const checked = ref(true);
+const indeterminate = ref(false);
 onMounted(() => {
-  if (checkbox.value !== undefined) {
-    checkbox.value.indeterminate = true;
-  }
+  watch(indeterminate, (isIndeterminate) => {
+    checkbox.value!.indeterminate = isIndeterminate;
+  });
+
+  watch(checked, () => {
+    indeterminate.value = false;
+  });
 });
 </script>
 
 <template>
   <form class="accent-color-experiment" @submit.prevent>
     <div class="text">
-      <label for="color">Accent Color</label>
-      <input id="color" v-model="color" type="text" />
+      <label for="accent-color">Accent Color</label>
+      <input
+        id="accent-color"
+        v-model="color"
+        type="text"
+        class="tw:form-input"
+      />
     </div>
 
     <div class="checkbox">
-      <input id="checkbox" ref="checkbox" type="checkbox" class="input" />
+      <input
+        id="checkbox"
+        ref="checkbox"
+        v-model="checked"
+        type="checkbox"
+        class="input tw:form-checkbox"
+      />
       <label for="checkbox">Checkbox</label>
+
+      <input
+        id="indeterminate"
+        v-model="indeterminate"
+        type="checkbox"
+        class="input tw:form-checkbox"
+      />
+      <label for="indeterminate">(Indeterminate)</label>
     </div>
 
     <div v-for="{ id, name } of fruits" :key="id" class="radio">
@@ -39,7 +64,7 @@ onMounted(() => {
         v-model="selectedFruitRadio"
         type="radio"
         name="fruit"
-        class="input"
+        class="input tw:form-radio"
         :value="id"
       />
       <label :for="id">{{ name }}</label>
@@ -90,52 +115,69 @@ onMounted(() => {
       <progress id="progress" max="100" value="70">70%</progress>
 
       <label for="text">text</label>
-      <input id="text" type="text" />
+      <input id="text" type="text" class="tw:form-input" />
+
+      <label for="textarea">textarea</label>
+      <textarea id="textarea" class="tw:form-textarea"></textarea>
 
       <label for="tel">tel</label>
-      <input id="tel" type="tel" />
+      <input id="tel" type="tel" class="tw:form-input" />
 
       <label for="email">email</label>
-      <input id="email" type="email" />
+      <input id="email" type="email" class="tw:form-input" />
 
       <label for="url">url</label>
-      <input id="url" type="url" />
+      <input id="url" type="url" class="tw:form-input" />
 
       <label for="number">number</label>
-      <input id="number" type="number" />
+      <input id="number" type="number" class="tw:form-input" />
 
       <label for="date">date</label>
-      <input id="date" type="date" />
+      <input id="date" type="date" class="tw:form-input" />
 
       <label for="time">time</label>
-      <input id="time" type="time" />
+      <input id="time" type="time" class="tw:form-input" />
 
       <label for="datetime-local">datetime-local</label>
-      <input id="datetime-local" type="datetime-local" />
+      <input id="datetime-local" type="datetime-local" class="tw:form-input" />
 
       <label for="week">week</label>
-      <input id="week" type="week" />
+      <input id="week" type="week" class="tw:form-input" />
 
       <label for="month">month</label>
-      <input id="month" type="month" />
+      <input id="month" type="month" class="tw:form-input" />
 
       <label for="color">color</label>
       <input id="color" type="color" />
 
       <label for="password">password</label>
-      <input id="password" type="password" autocomplete="off" />
+      <input
+        id="password"
+        type="password"
+        autocomplete="off"
+        class="tw:form-input"
+      />
 
-      <label for="submit">submit</label>
-      <input id="submit" type="submit" @click.prevent />
+      <label for="submit-input">submit (input)</label>
+      <input id="submit-input" type="submit" @click.prevent />
+
+      <label for="submit-button">submit (button)</label>
+      <button id="submit-button" type="submit" @click.prevent>Submit</button>
 
       <label for="search">search</label>
-      <input id="search" type="search" />
+      <input id="search" type="search" class="tw:form-input" />
 
-      <label for="button">button</label>
-      <input id="button" type="button" value="button" />
+      <label for="button-input">button (input)</label>
+      <input id="button-input" type="button" value="button" />
 
-      <label for="reset">reset</label>
-      <input id="reset" type="reset" @click.prevent />
+      <label for="button-button">button (button)</label>
+      <button id="button-button" type="button">button</button>
+
+      <label for="reset-input">reset (input)</label>
+      <input id="reset-input" type="reset" @click.prevent />
+
+      <label for="reset-button">reset (button)</label>
+      <button id="reset-button" type="reset" @click.prevent>Reset</button>
 
       <label for="file">file</label>
       <input id="file" type="file" />
