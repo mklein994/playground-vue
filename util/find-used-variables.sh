a25f8ecff343a38804ba2a5014e942bd5c515a73
Matthew Klein <mklein994@gmail.com>
2025-12-13 15:03:34 -0600

    Add a script for finding used static variables

---
+++ util/find-used-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pnpm run build:tokens -p json >&2
+
+comm -12 <(jq -r 'keys[] | "--\(.)"' src/assets/generated/variables.json | sort -u) \
+  <(git grep --untracked --no-heading -h --only-matching \
+    --perl-regexp -e '--pv-base-\w[-\w]+' \
+    ':/**/*.scss' \
+    ':/**/*.css' \
+    ':/**/*.vue' |
+    sort -u)
