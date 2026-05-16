d2be12378b020f459f26f3f911e0e1c87c04d4d6
Matthew Klein <mklein994@gmail.com>
2026-05-16 16:52:49 -0500

    Replace stylelint-config-tailwindcss with @dreamsicle.io/stylelint-config-tailwindcss
    This new version extends the language instead of simply ignoring the new
    functions, making it easier to merge the two. I only recently realized
    that the "extends" order mattered, and that configurations were actually
    overriding each other instead of combining. Now, with a fix from
    https://github.com/dreamsicle-io/stylelint-config-tailwindcss/pull/27,
    it actually knows the Tailwind "language", so to speak.

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,43 +1,25 @@
+import configTailwindCss from "@dreamsicle.io/stylelint-config-tailwindcss/dist/stylelint.config.mjs";
+
 /** @type {import("stylelint").Config} */
 export default {
   extends: [
     "stylelint-config-recommended",
-    "stylelint-config-tailwindcss",
     "stylelint-config-clean-order",
     "stylelint-config-recommended-vue",
+    "@dreamsicle.io/stylelint-config-tailwindcss",
   ],
+  languageOptions: {
+    syntax: {
+      atRules: {
+        plugin: { prelude: "<string>" },
+        ...configTailwindCss.languageOptions.syntax.atRules,
+      },
+    },
+  },
   overrides: [
     {
       files: ["**/*.vue", "*.vue", "**/*.css", "*.css"],
       ignoreFiles: "**/reset.css",
-      rules: {
-        "function-no-unknown": [
-          true,
-          {
-            ignoreFunctions: [
-              // Vue
-              "v-bind",
-
-              // Tailwind
-              "theme",
-            ],
-          },
-        ],
-        "declaration-property-value-no-unknown": [
-          true,
-          {
-            ignoreProperties: {
-              "/.*/": "/(?:v-bind|theme)\\(.+\\)/",
-            },
-          },
-        ],
-        "at-rule-no-unknown": [
-          true,
-          {
-            ignoreAtRules: ["custom-variant", "plugin", "reference"],
-          },
-        ],
-      },
     },
   ],
 };
