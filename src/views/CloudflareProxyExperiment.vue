92ac23b3079f44b179c363b8d15342d822c02dac
Matthew Klein <mklein994@gmail.com>
2026-05-20 22:33:44 -0500

    Add an experiment that uses the R2 proxy

---
+++ src/views/CloudflareProxyExperiment.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="cloudflare-proxy-experiment">
+    <img
+      :src="'/r2/mabc_hero.png'"
+      alt="Perspective picture of MABC looking at steeple"
+    />
+  </div>
+</template>
